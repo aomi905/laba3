d558cea7889a9e0dd94c3745a75b7a7156bffee8
Anna Suravets <hanna.s905@gmail.com>
2020-11-24 21:21:58 +0300

    Меню

--- src/bsu/rfact/java/laba3/MainFrame.java
+++ src/bsu/rfact/java/laba3/MainFrame.java
@@ -11,15 +11,31 @@ public  class  MainFrame extends Frame {
     private JFileChooser fileChooser = null;
     private JMenuItem saveToTextMenuItem,
             saveToGraphicsMenuItem,
-            saveToCSVMenuItem,
-            searchValueMenuItem,
-            searchSimilarValueMenuItem;
+            searchValueMenuItem;
+
     private JTextField textFieldFrom,
             textFieldTo,
             textFieldStep;
     private Box hBoxResult;
     private GornerTableCellRenderer renderer = new GornerTableCellRenderer();
     private GornerTableModel data;
+
+    public MainFrame(Double[] coefficients){
+        super("Tabulating a polynomial on a segment in 2 ways..");
+        this.coefficients = coefficients;
+        setSize(WIDTH, HEIGHT);
+        Toolkit kit = Toolkit.getDefaultToolkit();
+        setLocation((kit.getScreenSize().width - WIDTH) / 2,
+                (kit.getScreenSize().height - HEIGHT) / 2);
+        JMenuBar menuBar = new JMenuBar();
+        JFrame frame = new JFrame();
+        frame.setJMenuBar(menuBar);
+        JMenu fileMenu = new JMenu("File");
+        menuBar.add(fileMenu);
+        JMenu tablaMenu = new JMenu("Table");
+        menuBar.add(tablaMenu);
+
+    }
 
     public  static  void  main ( String [] args ) {
 
