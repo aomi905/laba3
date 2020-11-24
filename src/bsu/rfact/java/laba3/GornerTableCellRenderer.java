1b33f96dccb877b5cb30e1bf8f61085e643ea7d1
Anna Suravets <hanna.s905@gmail.com>
2020-11-24 20:16:04 +0300

    Класс GornerTableCellRenferer

---
+++ src/bsu/rfact/java/laba3/GornerTableCellRenderer.java
@@ -0,0 +1,14 @@
+package bsu.frct.java.lab3;
+
+import javax.swing.*;
+import javax.swing.table.TableCellRenderer;
+import java.awt.*;
+
+public class GornerTableCellRenderer {
+
+    @Override
+    public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus, int row, int column) {
+        return null;
+    }
+
+}
