6b215e34b03161092430929d7e55b7d66b231662
Anna Suravets <hanna.s905@gmail.com>
2020-11-24 20:03:32 +0300

    Поля класса

--- src/bsu/rfact/java/laba3/GornerTableModel.java
+++ src/bsu/rfact/java/laba3/GornerTableModel.java
@@ -4,6 +4,32 @@ import javax.swing.table.AbstractTableModel;
 
 
 public class GornerTableModel extends AbstractTableModel {
+
+    private Double[] coefficients;
+    private Double from,
+            to,
+            step;
+
+    public GornerTableModel(Double from, Double to,
+                            Double step, Double[] coefficients){
+        this.from = from;
+        this.to = to;
+        this.step = step;
+        this.coefficients = coefficients;
+    }
+
+    public Double getFrom() {
+        return from;
+    }
+
+    public Double getTo() {
+        return to;
+    }
+
+    public Double getStep() {
+        return step;
+    }
+
     @Override
     public int getRowCount() {
         return 0;
@@ -17,6 +43,16 @@ public class GornerTableModel extends AbstractTableModel {
     @Override
     public Object getValueAt(int rowIndex, int columnIndex) {
         return null;
+    }
+
+    @Override
+    public String getColumnName(int column) {
+        return super.getColumnName(column);
+    }
+
+    @Override
+    public Class<?> getColumnClass(int columnIndex) {
+        return super.getColumnClass(columnIndex);
     }
 }
 
